c2fd6fb20db67a3167f630fca16f45835747d930
faizahsayyid <faizah.sayyid@gmail.com>
2021-10-09 20:54:31 -0400

    inital commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.onionshop.onionstarter {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.onionshop to javafx.fxml;
+    exports com.onionshop;
+}
