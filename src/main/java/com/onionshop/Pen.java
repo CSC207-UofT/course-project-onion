ffba66809b895b558274dc054dbd2001d4f3310c
Tina Zhang <69278622+Gnahzanit@users.noreply.github.com>
2021-10-12 21:29:40 -0400

    pen done comment later

--- src/main/java/com/onionshop/Pen.java
+++ src/main/java/com/onionshop/Pen.java
@@ -2,5 +2,12 @@ package com.onionshop;
 
 public class Pen extends Brush{
 
+    private String brushShape;
+    private int brushSize;
 
+    public Pen(String brushShape, int brushSize) {
+        super(brushShape, brushSize);
+        this.brushShape = "round";
+        this.brushSize = 1;
+    }
 }
