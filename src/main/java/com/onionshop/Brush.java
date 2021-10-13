9053b430fb4030928cdc745cf90976e309827ec5
Tina Zhang <69278622+Gnahzanit@users.noreply.github.com>
2021-10-12 21:16:55 -0400

    The absolute bare minimum of brush abstract class :beer:

--- src/main/java/com/onionshop/Brush.java
+++ src/main/java/com/onionshop/Brush.java
@@ -1,5 +1,34 @@
 package com.onionshop;
 
-abstract class Brush {
-    
+public abstract class Brush {
+
+    /*
+     * Private instance variables
+     *      - a String named brushShape
+     *      - an int named brushSize
+     */
+
+    private String brushShape;
+    private int brushSize;
+
+    public Brush(String brushShape, int brushSize, String defaultColour, String currentColour) {
+        this.brushShape = brushShape;
+        this.brushSize = 1;
+    }
+
+    public String getBrushShape() {
+        return brushShape;
+    }
+
+    public int getBrushSize() {
+        return brushSize;
+    }
+
+    public void setBrushShape(String brushShape) {
+        this.brushShape = brushShape;
+    }
+
+    public void setBrushSize(int brushSize) {
+        this.brushSize = brushSize;
+    }
 }
