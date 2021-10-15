c9adf9e919dbfa5f57b774cb5070cd5c8f3f95f1
Tina Zhang <69278622+Gnahzanit@users.noreply.github.com>
2021-10-15 18:09:08 -0400

    ToolStatemanager Implemented someone wanna tell me if I'm doing the right thing

--- src/main/java/com/onionshop/ToolStateManager.java
+++ src/main/java/com/onionshop/ToolStateManager.java
@@ -1,7 +1,43 @@
 package com.onionshop;
 
 public class ToolStateManager {
+    /**
+     * This class holds the current state of the tools and colours used on canvas, and updates them according to
+     * user inputs.
+     */
 
+    private Tool currentToolState;
+    private Colour currentColourState;
 
+    public ToolStateManager(Tool currentToolState, Colour currentColourState) {
+        this.currentToolState = currentToolState;
+        this.currentColourState = currentColourState;
+    }
 
+    public void updateCurrentToolState(String brushShape, int brushSize) {
+        if (this.currentToolState instanceof Brush) {
+            ((Brush) this.currentToolState).setBrushShape(brushShape);
+            ((Brush) this.currentToolState).setBrushSize(brushSize);
+        }
+    }
+
+    public void updateCurrentColourState(int[] changes) {
+        this.currentColourState.modify(changes);
+    }
+
+    public void setCurrentToolState(Tool currentToolState) {
+        this.currentToolState = currentToolState;
+    }
+
+    public void setCurrentColourState(Colour currentColourState) {
+        this.currentColourState = currentColourState;
+    }
+
+    public Tool getCurrentToolState() {
+        return currentToolState;
+    }
+
+    public Colour getCurrentColourState() {
+        return currentColourState;
+    }
 }
