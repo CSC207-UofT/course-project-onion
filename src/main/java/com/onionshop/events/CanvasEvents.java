7415236aff80437c5b5bbdb3e891ff3cbefc742c
Runce Zhang <runce001@gmail.com>
2021-10-16 12:14:20 -0400

    Implemented CanvasEvents.java

--- src/main/java/com/onionshop/events/CanvasEvents.java
+++ src/main/java/com/onionshop/events/CanvasEvents.java
@@ -1,4 +1,25 @@
 package com.onionshop.events;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+
+/**
+ *
+ */
 public class CanvasEvents {
+    @FXML
+    private Canvas projectDrawing;
+
+    /**
+     * Takes in a mouse event and pass the x,y coordinates down
+     */
+    public double[] DrawingEvent(MouseEvent canvasMouseClicked){
+        double[] arr = new double[2];
+        arr[0] = canvasMouseClicked.getX();
+        arr[1] = canvasMouseClicked.getY();
+
+        return arr;
+    }
 }
