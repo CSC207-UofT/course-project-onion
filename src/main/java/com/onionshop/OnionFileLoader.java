0657405325cdd734b0cf1934d9b0f7f0ea82ac19
Finn Williams <52074771+FritzFeynman@users.noreply.github.com>
2021-10-29 20:44:20 -0400

    Updated OnionFileLoader according to what our TA said (split up the big loader method) and added corresponding testing, also minor formatting changes

--- src/main/java/com/onionshop/OnionFileLoader.java
+++ src/main/java/com/onionshop/OnionFileLoader.java
@@ -14,9 +14,10 @@ import java.util.Objects;
 public class OnionFileLoader {
 
     /**
-        Saves Project instance serialization to .onion file type
-        @param project: the Project instance of which a serialization will be saved
-        @return true if save was successful, false if the save failed
+     * Saves Project instance serialization to .onion file type
+     *
+     * @param project the Project instance of which a serialization will be saved
+     * @return true if save was successful, false if the save failed
      */
     public static boolean saveProject(Project project) throws IOException {
         try {
@@ -37,10 +38,11 @@ public class OnionFileLoader {
 
 
     /**
-     Returns whether the path is a valid directory (meaning it exists, is readable and
-     writable)
-     @param pathString: the path to be checked
-     @return whether the path is a valid directory
+     * Returns whether the path is a valid directory (meaning it exists, is readable and
+     * writable)
+     *
+     * @param pathString the path to be checked
+     * @return whether the path is a valid directory
      */
     public static boolean isDirectoryValid(String pathString) {
         File file = new File(pathString);
@@ -50,9 +52,10 @@ public class OnionFileLoader {
 
     /**
      * Return whether the file already exists
-     Saves Project instance serialization to .onion file type
-     @param pathString: the path to be checked
-     @return whether the file already exists in the specified pathString
+     * Saves Project instance serialization to .onion file type
+     *
+     * @param pathString the path to be checked
+     * @return whether the file already exists in the specified pathString
      */
     public static boolean doesFileAlreadyExist(String pathString) {
         File file = new File(pathString);
@@ -62,10 +65,24 @@ public class OnionFileLoader {
     /**
      * Converts a .onion file to a Project instance
      *
-     * @param path: the location of the .onion file
-     * @return: returns a Project instance if there are no files errors, returns null if there are errors
+     * @param path the location of the project .onion file
+     * @return returns a Project instance if there are no files errors, returns null if there are errors
      */
-    public static Project loadProject(String path) {
+    public static Project loadProject(String path) throws Exception {
+        String[] lines = getFileLines(path);
+        Project loadedProject = generateProjectInstance(lines);
+        loadedProject.colourPalette = generateColourPalette(lines);
+        loadedProject.drawingCanvas = generatePixelArray(loadedProject.width, loadedProject.height, lines);
+        return loadedProject;
+    }
+
+    /**
+     * Reads .onion files and coverts them to a programmatic format
+     *
+     * @param path the path of the file to be read
+     * @return an array of strings where arr[i] is the ith line of the file
+     */
+    private static String[] getFileLines(String path) {
         String line;
         ArrayList<String> lines = new ArrayList<>();
 
@@ -78,6 +95,8 @@ public class OnionFileLoader {
                 lines.add(line);
             }
             bufferedReader.close();
+            String[] linesArray = new String[lines.size()];
+            return lines.toArray(linesArray);
         } catch (FileNotFoundException ex) {
             System.out.println("Unable to open file '" + path + "'");
             return null;
@@ -85,26 +104,34 @@ public class OnionFileLoader {
             System.out.println("Error reading file '" + path + "'");
             return null;
         }
+    }
 
-        //CREATING PROJECT INSTANCE
+    /**
+     * Helper method to loadProject
+     * Creates a project instance from the lines of a file in onion format
+     *
+     * @param lines array of Strings where array[i] is the i-th line of a file
+     * @return Project instane with width and height read from lines and empty path
+     */
+    private static Project generateProjectInstance(String[] lines) {
+        int width = Integer.parseInt(lines[1].substring(lines[1].indexOf(':') + 1));
+        int height = Integer.parseInt(lines[2].substring(lines[2].indexOf(':') + 1));
+        return new Project("", width, height);
+    }
 
-        //getting width and height
-        int lineNumber = 1;
-        line = lines.get(lineNumber);
-        int width = Integer.parseInt(line.substring(line.indexOf(':') + 1));
-        lineNumber++;
-        line = lines.get(lineNumber);
-        int height = Integer.parseInt(line.substring(line.indexOf(':') + 1));
-        lineNumber += 2;
-
-        Project loadedProject = new Project(path, width, height);
-        loadedProject.updatePath(path);
-
-        //getting saved colours
+    /**
+     * Helper method to loadProject
+     * Creates a Colour array from the saved colours read from lines
+     *
+     * @param lines array of Strings where array[i] is the i-th line of a file
+     * @return array of saved colours read from lines
+     */
+    private static Colour[] generateColourPalette(String[] lines) throws Exception {
         ArrayList<Colour> colourPalette = new ArrayList<>();
-        while (!Objects.equals(line = lines.get(lineNumber), "[pixels]")) {
+        int lineNumber = getIndexOfString("[saved colours]", lines) + 1;
+        String line;
+        while (!Objects.equals(line = lines[lineNumber], "[pixels]")) {
             String name = line.substring(0, line.indexOf(':'));
-            System.out.println(line);
             String stringRGB = line.substring(line.indexOf(':') + 1);
             int[] RGB = new int[]{
                     Integer.parseInt(stringRGB.substring(0, stringRGB.indexOf(','))),
@@ -114,14 +141,26 @@ public class OnionFileLoader {
             colourPalette.add(new Colour(name, RGB));
             lineNumber++;
         }
-        lineNumber++;
-        //getting saved drawing canvas
+        return colourPalette.toArray(new Colour[0]); //This might not work
+    }
 
+    /**
+     * Helper method to loadProject;
+     * Reads a 1D array of strings coverts them to ints and then maps them to a 2D array
+     * representing the
+     *
+     * @param width  width of canvas in pixels
+     * @param height height of canvas in pixels
+     * @param lines  array of Strings where array[i] is the i-th line of a file
+     * @return returns 2D mapped array of Pixels where arr[x][y] is a pixel on the screen at (x, y)
+     */
+    private static Pixel[][] generatePixelArray(int width, int height, String[] lines) throws Exception {
         Pixel[][] drawingCanvas = new Pixel[width][height];
-        //NOTE: may want to replace this with ArrayList mapping algorithm - Finn
+        String line;
+        int lineNumber = getIndexOfString("[pixels]", lines) + 1;
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                line = lines.get(lineNumber);
+                line = lines[lineNumber];
                 int[] RGB = new int[]{
                         Integer.parseInt(line.substring(0, line.indexOf(','))),
                         Integer.parseInt(line.substring(line.indexOf(',') + 1, line.lastIndexOf(','))),
@@ -131,10 +170,28 @@ public class OnionFileLoader {
                 lineNumber++;
             }
         }
-        assert (Objects.equals(lines.get(lineNumber), "[end]"));
-        loadedProject.colourPalette = colourPalette.toArray(loadedProject.colourPalette);
-        loadedProject.drawingCanvas = drawingCanvas;
+        return drawingCanvas;
+    }
 
-        return loadedProject;
+    /**
+     * finds and returns the index of the first appearance of a String in an array
+     *
+     * @param str   the string of which you are finding the index of the first appearance of
+     * @param lines array of Strings where array[i] is the i-th line of a file
+     * @return index of str appearance
+     * @throws Exception throws exception str does not appear in the lines array
+     */
+    public static int getIndexOfString(String str, String[] lines) throws Exception {
+        int lineNumber = -1;
+        for (int i = 0; i < lines.length; i++) {
+            if (Objects.equals(lines[i], str)) {
+                lineNumber = i;
+                break;
+            }
+        }
+        if (lineNumber == -1) {
+            throw new Exception("Array does not contain element: " + str);
+        } //may want more specific error
+        return lineNumber;
     }
 }
