a677dd1a261b5186d14661a5e4ecef550e697d11
CynthiaLiNingxin <ningxin.li@mail.utoronto.ca>
2021-11-04 01:35:47 -0400

    save and try

--- src/test/java/com/onionshop/managers/UndoRedoManagerTest.java
+++ src/test/java/com/onionshop/managers/UndoRedoManagerTest.java
@@ -1,15 +1,12 @@
 package com.onionshop.managers;
 
-import com.onionshop.entities.Pen;
 import com.onionshop.entities.Project;
 import com.onionshop.managers.DrawingManager;
 import com.onionshop.managers.UndoRedoManager;
-import com.onionshop.controllers.NewProjectController;
 import org.junit.Test;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -18,7 +15,11 @@ public class UndoRedoManagerTest {
     private final DrawingManager Drawing = new DrawingManager();
     private final UndoRedoManager UndoRedo = new UndoRedoManager();
 
-
+    /**
+     * Set up a drawn canvas before each test.
+     *
+     * @throws Exception
+     */
     @BeforeEach
     public void setUp() throws Exception {
         p = new Project("test", 2, 2);
@@ -39,6 +40,46 @@ public class UndoRedoManagerTest {
                 "[pixels]",
                 "0,0,0",
                 "255,255,255",
+                "255,255,255",
+                "255,255,255",
+                "[end]"};
+        assertEquals(project, expected);
+    }
+
+    @Test
+    public void testUndoTwice() {
+        UndoRedo.undo();
+        UndoRedo.undo();
+        String[] project = p.serialize();
+        String[] expected = {
+                "[dimensions]",
+                "width:2",
+                "height:2",
+                "[saved colours]",
+                "black:0,0,0",
+                "[pixels]",
+                "255,255,255",
+                "255,255,255",
+                "255,255,255",
+                "255,255,255",
+                "[end]"};
+        assertEquals(project, expected);
+    }
+
+    @Test
+    public void testRedo() {
+        UndoRedo.undo();
+        UndoRedo.redo();
+        String[] project = p.serialize();
+        String[] expected = {
+                "[dimensions]",
+                "width:2",
+                "height:2",
+                "[saved colours]",
+                "black:0,0,0",
+                "[pixels]",
+                "0,0,0",
+                "0,0,0",
                 "255,255,255",
                 "255,255,255",
                 "[end]"};
