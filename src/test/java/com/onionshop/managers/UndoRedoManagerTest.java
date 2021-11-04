be85927d4dffe43b8ba4db374f947fafb5662f61
CynthiaLiNingxin <ningxin.li@mail.utoronto.ca>
2021-11-04 01:05:55 -0400

    save and try

--- src/test/java/com/onionshop/managers/UndoRedoManagerTest.java
+++ src/test/java/com/onionshop/managers/UndoRedoManagerTest.java
@@ -1,8 +1,11 @@
 package com.onionshop.managers;
 
+import com.onionshop.entities.Pen;
 import com.onionshop.entities.Project;
+import com.onionshop.managers.DrawingManager;
 import com.onionshop.managers.UndoRedoManager;
 import com.onionshop.controllers.NewProjectController;
+import org.junit.Test;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -12,9 +15,33 @@ import java.io.IOException;
 
 public class UndoRedoManagerTest {
     Project p;
+    private final DrawingManager Drawing = new DrawingManager();
+    private final UndoRedoManager UndoRedo = new UndoRedoManager();
+
 
     @BeforeEach
     public void setUp() throws Exception {
         p = new Project("test", 2, 2);
+        Drawing.updateCanvasAfterStroke(1,1);
+        Drawing.updateCanvasAfterStroke(1,2);
+    }
+
+    @Test
+    public void testUndo() {
+        UndoRedo.undo();
+        String[] project = p.serialize();
+        String[] expected = {
+                "[dimensions]",
+                "width:2",
+                "height:2",
+                "[saved colours]",
+                "black:0,0,0",
+                "[pixels]",
+                "0,0,0",
+                "255,255,255",
+                "255,255,255",
+                "255,255,255",
+                "[end]"};
+        assertEquals(project, expected);
     }
 }
