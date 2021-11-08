b7f455da523b66c92b2f1663c9e094e500b10032
Tina Zhang <69278622+Gnahzanit@users.noreply.github.com>
2021-11-08 15:39:04 -0500

    line shape tool completed

--- src/main/java/com/onionshop/entities/Line.java
+++ src/main/java/com/onionshop/entities/Line.java
@@ -1,5 +1,7 @@
 package com.onionshop.entities;
 
+import java.util.Collections;
+
 public class Line extends Shape implements Tool {
 
     public Line(int brushSize) {
@@ -7,12 +9,14 @@ public class Line extends Shape implements Tool {
         this.calculateEffectedPixels();
     }
 
-    private double lineCalculationFormula(int x, int[] distances) {
+    private double[] lineCalculationFormula(int coordinateValue, int[] distances) {
+        double[] equation = new double[2];
         double m = calculateSlope(distances);
 
-        System.out.println("what is the slope mate is it working " + m);
+        equation[0] = (double) coordinateValue * m;
+        equation[1] = (double) coordinateValue / m;
 
-        return x * m;
+        return equation;
     }
 
     public void calculateEffectedPixels() {
@@ -21,8 +25,8 @@ public class Line extends Shape implements Tool {
             pixelsEffectedByShape = new int[121][2];
 
             int counter = 0;
-            for (int x = -5; x < 6; x++) {
-                for (int y = -5; y < 6; y++) {
+            for (int x = -2; x < 3; x++) {
+                for (int y = -2; y < 3; y++) {
                     this.pixelsEffectedByShape[counter][0] = x;
                     this.pixelsEffectedByShape[counter][1] = y;
                     counter++;
@@ -33,43 +37,49 @@ public class Line extends Shape implements Tool {
 
         } else if (drawStage == 2) {
 
-            System.out.println("ran?");
-
-            System.out.println("did the coordinates pass start " + this.startingCoordinate[0]);
-            System.out.println("did the coordinates pass start " + this.startingCoordinate[1]);
-
-            System.out.println("did the coordinates pass end " + this.endingCoordinate[0]);
-            System.out.println("did the coordinates pass end " + this.endingCoordinate[1]);
-
-            pixelsEffectedByShape = new int[121][2];
-
             // import the start and end coordinate distances
             int[] distances = calculateStartEndDistance();
 
-            System.out.println("What's the distance did it work " + distances[0] + " and " + distances[1]);
+            pixelsEffectedByShape = new int[2 * Math.max(Math.max(startingCoordinate[0], startingCoordinate[1]),
+                    Math.max(endingCoordinate[0], endingCoordinate[1]))][2];
 
             // a counter for exiting the while loop when we arrive at the end coordinate
             int xCounter = 0;
 
             // while loop stop condition: when counter arrives at the x distance between start and end coordinates
-            while (xCounter < Math.abs(distances[0])) {
+            while (Math.abs(xCounter) < Math.abs(distances[0])) {
+                this.pixelsEffectedByShape[Math.abs(xCounter)][0] = startingCoordinate[0] + xCounter;
+                this.pixelsEffectedByShape[Math.abs(xCounter)][1] =
+                        (int) (startingCoordinate[1] + lineCalculationFormula(xCounter, distances)[0]);
 
-                System.out.println("Lets see if this works " + lineCalculationFormula(xCounter, distances));
-                System.out.println(xCounter);
+                if (distances[0] > 0) {
+                    xCounter--;
+                } else {
+                    xCounter++;
+                }
 
-                this.pixelsEffectedByShape[xCounter][0] = startingCoordinate[0] + xCounter;
-                this.pixelsEffectedByShape[xCounter][1] =
-                        (int) (startingCoordinate[1] + lineCalculationFormula(xCounter, distances));
-                xCounter++;
             }
 
-            for (int beef = 0; beef < pixelsEffectedByShape.length; beef++) {
-                System.out.println("loopin x " + pixelsEffectedByShape[beef][0]);
-                System.out.println("loopin y " + pixelsEffectedByShape[beef][1]);
+            // a counter for exiting the while loop when we arrive at the end coordinate
+            int yCounter = 0;
+
+            // while loop stop condition: when counter arrives at the y distance between start and end coordinates
+            while (Math.abs(yCounter) < Math.abs(distances[1])) {
+
+                this.pixelsEffectedByShape[Math.abs(xCounter + yCounter)][0] =
+                        (int) (lineCalculationFormula(yCounter, distances)[1] + startingCoordinate[0]);
+                this.pixelsEffectedByShape[Math.abs(xCounter + yCounter)][1] = startingCoordinate[1] + yCounter;
+
+                if (distances[1] > 0) {
+                    yCounter--;
+                } else {
+                    yCounter++;
+                }
+
             }
 
-            assert Math.abs(this.pixelsEffectedByShape[xCounter - 1][0] - endingCoordinate[0]) < 5;
-            assert Math.abs(this.pixelsEffectedByShape[xCounter - 1][1] - endingCoordinate[1]) < 5;
+//            assert Math.abs(this.pixelsEffectedByShape[Math.abs(xCounter) - 1][0] - endingCoordinate[0]) < 5;
+//            assert Math.abs(this.pixelsEffectedByShape[Math.abs(xCounter) - 1][1] - endingCoordinate[1]) < 5;
         }
     }
 
--- src/main/java/com/onionshop/entities/Shape.java
+++ src/main/java/com/onionshop/entities/Shape.java
@@ -25,7 +25,7 @@ public abstract class Shape implements Tool {
     public int[][] draw(Project currentCanvas, Colour currentColour, int x, int y) {
         // Creating a new array to store the pixels that are updated in this method. These will then
         // Be sent back up to javafx to be rendered on the canvas.
-        int[][] pixelsToUpdate = new int[pixelsEffectedByShape.length][2];
+        int[][] pixelsToUpdate = new int[Math.max(currentCanvas.width, currentCanvas.height)][2];
 
         if (drawStage == 1) {
 
@@ -45,43 +45,21 @@ public abstract class Shape implements Tool {
                 }
             }
 
-            System.out.println("first what " + drawStage);
-
             drawStage++;
-
-            System.out.println("should be two now what " + drawStage);
-
-            System.out.println("first " + x);
-            System.out.println("first " + y);
 
             this.startingCoordinate[0] = this.startingCoordinate != null ? x : this.startingCoordinate[0];
             this.startingCoordinate[1] = this.startingCoordinate != null ? y : this.startingCoordinate[1];
 
-            System.out.println("first should be x what " + this.startingCoordinate[0]);
-            System.out.println("first should be y what " + this.startingCoordinate[1]);
-
         } else if (drawStage == 2) {
-
-            System.out.println("should still be two now what " + drawStage);
-
-            System.out.println("second " + x);
-            System.out.println("second " + y);
 
             this.endingCoordinate[0] = this.endingCoordinate != null ? x : this.endingCoordinate[0];
             this.endingCoordinate[1] = this.endingCoordinate != null ? y : this.endingCoordinate[1];
-
-            System.out.println("second should be x what " + this.endingCoordinate[0]);
-            System.out.println("second should be y what " + this.endingCoordinate[1]);
 
             // stage 2, return pixelsEffectedByShape to be the line and its exact coordinate we want to plot
             this.calculateEffectedPixels();
 
             // pixel is getting the exact amount of pixels we have to plot in the pixelsEffectedByShape array
             for (int pixel = 0; pixel < pixelsEffectedByShape.length; pixel++) {
-
-                System.out.println("calculated values doing alright? " + pixelsEffectedByShape[pixel][0]);
-                System.out.println("calculated values doing alright? " + pixelsEffectedByShape[pixel][1]);
-
                 // Check if the pixels are in the canvas, we don't need to add pixelsEffectedByShape onto x or y since
                 // the calculation done in calculateEffectedPixels already computed the exact coordinates
                 if (pixelsEffectedByShape[pixel][0] > 0  // x > 0
@@ -97,7 +75,8 @@ public abstract class Shape implements Tool {
                 }
 
             }
-            drawStage++;
+            drawStage = 0;
+            this.calculateEffectedPixels();
         }
 
         return pixelsToUpdate;
