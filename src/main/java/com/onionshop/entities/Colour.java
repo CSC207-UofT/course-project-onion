cc4149809c45377f279b508929324b88acf1a3c3
faizahsayyid <faizah.sayyid@gmail.com>
2021-11-12 00:03:49 -0500

    fix: onion file loader test fix

--- src/main/java/com/onionshop/entities/Colour.java
+++ src/main/java/com/onionshop/entities/Colour.java
@@ -12,11 +12,11 @@ public class Colour extends Pixel {
     //name of the colour, defined by user of default
     public String name;
 
-    /*
-    Creates instance of Colour
+    /**
+        Creates instance of Colour
 
-    @param name: see name instance variable
-    @param RGB: RGB value of Colour
+        @param name: see name instance variable
+        @param RGB: RGB value of Colour
      */
     public Colour(String name, int[] RGB) {
         super(RGB);
@@ -38,10 +38,9 @@ public class Colour extends Pixel {
             }
         }
     }
-    /*
-    Safely modifies RGB values of Pixel instance
-
-    @param changes: the changes of each RGB value (e.g. {10, 0, 0} would increase the red RGB value by 10)
+    /**
+        Safely modifies RGB values of Pixel instance
+        @param changes: the changes of each RGB value (e.g. {10, 0, 0} would increase the red RGB value by 10)
      */
     public void modify(int[] changes) {
         for (int i = 0; i < 3; i++) {
