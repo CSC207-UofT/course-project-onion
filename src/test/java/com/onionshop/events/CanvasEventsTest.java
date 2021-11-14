774b5e173f64b58a517e0688142aecf74a7a15d1
camm17 <ammcourtney0@gmail.com>
2021-11-14 15:35:26 -0500

    added unit testing for CanvasEvents and DrawingManager

---
+++ src/test/java/com/onionshop/events/CanvasEventsTest.java
@@ -0,0 +1,104 @@
+package com.onionshop.events;
+
+import com.onionshop.controllers.Tools;
+import com.onionshop.entities.*;
+import com.onionshop.managers.DrawingManager;
+import com.onionshop.managers.ProjectManager;
+import com.onionshop.managers.ToolStateManager;
+import javafx.scene.paint.Color;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CanvasEventsTest {
+    /**
+     * This is the test suite for the CanvasEvents class
+     */
+    static String savePath;
+    static CanvasEvents canvasEvents;
+    static DrawingManager drawingManager;
+
+    @TempDir
+    static Path tempDir;
+
+    /**
+     * This function creates a temporary file to set up the project before each test. It also creates a new project,
+     * CanvasEvents, and DrawingManager class with a clear canvas and color palette.
+     *
+     * @throws Exception
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
+        Path tempFilePath = Files.createFile(tempDir.resolve("testFile1"));
+        savePath = tempFilePath.toString();
+        NewProjectEvent newProjectEvent = new NewProjectEvent("TestProject", savePath, 1000,
+                500);
+        ProjectManager.getInstance().newProject(newProjectEvent);
+        drawingManager = new DrawingManager();
+        canvasEvents = new CanvasEvents(drawingManager);
+    }
+
+    /**
+     * This deletes the temporary file for the next test
+     *
+     * @throws Exception
+     */
+    @AfterEach
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(tempDir.resolve("testFile1"));
+    }
+
+    /**
+     * This tests if passed in Colors are correctly processed by the testProcessSelectedColour function
+     */
+    @Test
+    public void testProcessSelectedColour() {
+        int conversionValue = 255;
+        Color testColor = new Color(1.0, .7, .25, 1.0);
+        int[] colourArray = new int[]{(int)(testColor.getRed()*conversionValue ),
+                (int)(testColor.getGreen()*conversionValue ), (int)(testColor.getBlue()*conversionValue )};
+        canvasEvents.processSelectedColour(testColor);
+        assertArrayEquals(ToolStateManager.getInstance().getCurrentColourState().getRGB(), colourArray);
+    }
+
+    /**
+     * This method tests that the correct Color hex value is returned and that the colour is correctly added to the
+     * backend palette
+     */
+    @Test
+    public void testProcessColourToAddToPalette() {
+        int conversionValue = 255;
+        Color testColor = new Color(0.811, 0.188, 0.188, 1.0);
+        int[] colourArray = new int[]{(int)(testColor.getRed()*conversionValue ),
+                (int)(testColor.getGreen()*conversionValue ), (int)(testColor.getBlue()*conversionValue )};
+
+        String hexColorValue = String.format("#%02x%02x%02x", colourArray[0], colourArray[1], colourArray[2]);
+        String testColorString = canvasEvents.processColourToAddToPalette(testColor);
+
+        assertEquals(testColorString, hexColorValue);
+        assertArrayEquals(ProjectManager.getInstance().getCurrentProject().getColourPalette()
+                .getColourByIndex(0).getRGB(), colourArray);
+    }
+
+    /**
+     * This method tests that each tools is correctly selected by the setTool method.
+     */
+    @Test
+    public void testSetTool() {
+        canvasEvents.setTool(Tools.LINE, 20);
+        assert(ToolStateManager.getInstance().getCurrentToolState() instanceof Line);
+        canvasEvents.setTool(Tools.PEN, 43);
+        assert(ToolStateManager.getInstance().getCurrentToolState() instanceof Pen);
+        canvasEvents.setTool(Tools.RECTANGLE, 34);
+        assert(ToolStateManager.getInstance().getCurrentToolState() instanceof Rectangle);
+        canvasEvents.setTool(Tools.ERASER, 23);
+        assert(ToolStateManager.getInstance().getCurrentToolState() instanceof Eraser);
+        canvasEvents.setTool(Tools.CIRCLE, 23);
+        assert(ToolStateManager.getInstance().getCurrentToolState() instanceof Circle);
+    }
+}
---
+++ src/test/java/com/onionshop/managers/DrawingManagerTest.java
@@ -0,0 +1,207 @@
+package com.onionshop.managers;
+
+import com.onionshop.entities.*;
+import com.onionshop.events.NewProjectEvent;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class DrawingManagerTest {
+    /**
+     * This is the test suite for the DrawingManager class.
+     */
+    static String savePath;
+    static DrawingManager drawingManager;
+
+    @TempDir
+    static Path tempDir;
+
+    /**
+     * This function creates a temporary file to set up the project before each test. It also creates a new project and
+     * DrawingManager with a clear canvas and color palette.
+     *
+     * @throws Exception
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
+        Path tempFilePath = Files.createFile(tempDir.resolve("testFile1"));
+        savePath = tempFilePath.toString();
+        NewProjectEvent newProjectEvent = new NewProjectEvent("TestProject", savePath, 1000,
+                500);
+        ProjectManager.getInstance().newProject(newProjectEvent);
+        ArrayList<Colour> colours = new ArrayList<Colour>();
+        ColourPalette colourPalette = new ColourPalette(colours);
+        ProjectManager.getInstance().getCurrentProject().setColourPalette(colourPalette);
+        drawingManager = new DrawingManager();
+    }
+
+    /**
+     * This deletes the temporary file for the next test
+     *
+     * @throws Exception
+     */
+    @AfterEach
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(tempDir.resolve("testFile1"));
+    }
+
+    /**
+     * This functions tests whether the correct pixels are edited when x, y coordinates are handed to the drawingManger's
+     * updateCanvasAfterStroke function.
+     */
+    @Test
+    public void testUpdateCanvasAfterStroke() {
+        int brushSize = 10;
+        int[] rgb = new int[]{123, 230, 3};
+        Colour testColour = new Colour("Test", rgb);
+        ToolStateManager.getInstance().setCurrentToolState(new Pen("round", brushSize));
+        ToolStateManager.getInstance().setCurrentColourState(testColour);
+        int x = 39;
+        int y = 100;
+        drawingManager.updateCanvasAfterStroke(x, y);
+        Project currentProject = ProjectManager.getInstance().getCurrentProject();
+        for (int i = x - brushSize; i < x + brushSize; i++) {
+            for (int j = y - brushSize; j < y + brushSize; j++) {
+                assertArrayEquals(currentProject.getPixelByCoord(i, j).getRGB(), rgb);
+            }
+        }
+    }
+
+    /**
+     * This function tests whether the toolStateManager is correctly updated with a new colour by the drawingManager's
+     * updateSelectedColour function.
+     */
+    @Test
+    public void testUpdateSelectedColour() {
+        int[] rgb = new int[]{244, 0, 130};
+        Colour testColour = new Colour("Test", rgb);
+        drawingManager.updateSelectedColour(testColour);
+        int[] currentToolRgb = ToolStateManager.getInstance().getCurrentColourState().getRGB();
+        assertArrayEquals(currentToolRgb, rgb);
+    }
+
+    /**
+     * This function tests whether colours are correctly added to the colour palette by the addColourToPalette function
+     */
+    @Test
+    public void testAddColourToPalette() {
+        String testColourHex = "#062488";
+        int[] rgb = new int[]{6, 36, 136};
+        Colour testColour = new Colour(testColourHex, rgb);
+        drawingManager.addColourToPalette(testColour);
+        int[] currentPaletteRGB = ProjectManager.getInstance().getCurrentProject().getColourPalette().
+                getColourByIndex(0).getRGB();
+        assertArrayEquals(currentPaletteRGB, rgb);
+
+    }
+
+    /**
+     * This function tests whether the correct colour is returned by the selectColourFromPalette function if the colour
+     * is in the palette.
+     */
+    @Test
+    public void testSelectColourFromPaletteColourFound() {
+        String testColourHex = "#b5a1a1";
+        int[] rgb = new int[]{181, 161, 161};
+        Colour testColour = new Colour(testColourHex, rgb);
+        String testColourHex2 = "#21424a";
+        int[] rgb2 = new int[]{33, 66, 74};
+        Colour testColour2 = new Colour(testColourHex2, rgb2);
+        String testColourHex3 = "#bce0c3";
+        int[] rgb3 = new int[]{188, 224, 195};
+        Colour testColour3 = new Colour(testColourHex3, rgb3);
+
+        drawingManager.addColourToPalette(testColour);
+        drawingManager.addColourToPalette(testColour2);
+        drawingManager.addColourToPalette(testColour3);
+
+        int[] currentPaletteRGB = drawingManager.selectColourFromPalette(testColourHex2);
+        assertArrayEquals(currentPaletteRGB, rgb2);
+    }
+
+    /**
+     * This function tests if Black is correctly returned when a String for a colour not in the palette is passed in to
+     * the selectColourFromPalette function.
+     */
+    @Test
+    public void testSelectColourFromPaletteColourNotFound() {
+        String testColourHex = "#b5a1a1";
+        int[] rgb = new int[]{181, 161, 161};
+        Colour testColour = new Colour(testColourHex, rgb);
+        String testColourHex2 = "#21424a";
+        int[] rgb2 = new int[]{33, 66, 74};
+        Colour testColour2 = new Colour(testColourHex2, rgb2);
+        String testColourHex3 = "#bce0c3";
+        int[] rgb3 = new int[]{188, 224, 195};
+        Colour testColour3 = new Colour(testColourHex3, rgb3);
+
+        drawingManager.addColourToPalette(testColour);
+        drawingManager.addColourToPalette(testColour2);
+        drawingManager.addColourToPalette(testColour3);
+
+        int[] currentPaletteRGB = drawingManager.selectColourFromPalette("#ba2010");
+        assertArrayEquals(currentPaletteRGB, new int[]{0, 0, 0});
+    }
+
+    /**
+     * This function tests if colours are correctly removed from the palette by the removeColourFromPalette function
+     */
+    @Test
+    public void testRemoveColourFromPaletteColourFound() {
+        String testColourHex = "#b5a1a1";
+        int[] rgb = new int[]{181, 161, 161};
+        Colour testColour = new Colour(testColourHex, rgb);
+        String testColourHex2 = "#21424a";
+        int[] rgb2 = new int[]{33, 66, 74};
+        Colour testColour2 = new Colour(testColourHex2, rgb2);
+        String testColourHex3 = "#bce0c3";
+        int[] rgb3 = new int[]{188, 224, 195};
+        Colour testColour3 = new Colour(testColourHex3, rgb3);
+
+        drawingManager.addColourToPalette(testColour);
+        drawingManager.addColourToPalette(testColour2);
+        drawingManager.addColourToPalette(testColour3);
+
+        drawingManager.removeColourFromPalette(testColourHex);
+        int[] currentPaletteRGB = drawingManager.selectColourFromPalette(testColourHex);
+        assertArrayEquals(currentPaletteRGB, new int[]{0, 0, 0});
+    }
+
+    /**
+     * This function tests if the removeColourFromPalette function correctly handles the case where a colour not in the
+     * palette is passed in to the function. It does this by ensuring all colours added to the palette previously are
+     * still in the palette after the function is run.
+     */
+    @Test
+    public void testRemoveColourFromPaletteColourNotFound() {
+        String testColourHex = "#b5a1a1";
+        int[] rgb = new int[]{181, 161, 161};
+        Colour testColour = new Colour(testColourHex, rgb);
+        String testColourHex2 = "#21424a";
+        int[] rgb2 = new int[]{33, 66, 74};
+        Colour testColour2 = new Colour(testColourHex2, rgb2);
+        String testColourHex3 = "#bce0c3";
+        int[] rgb3 = new int[]{188, 224, 195};
+        Colour testColour3 = new Colour(testColourHex3, rgb3);
+
+        drawingManager.addColourToPalette(testColour);
+        drawingManager.addColourToPalette(testColour2);
+        drawingManager.addColourToPalette(testColour3);
+
+        drawingManager.removeColourFromPalette("#1024ac");
+        int[] currentPaletteRGB1 = drawingManager.selectColourFromPalette(testColourHex);
+        int[] currentPaletteRGB2 = drawingManager.selectColourFromPalette(testColourHex2);
+        int[] currentPaletteRGB3 = drawingManager.selectColourFromPalette(testColourHex3);
+        assertArrayEquals(currentPaletteRGB1, rgb);
+        assertArrayEquals(currentPaletteRGB2, rgb2);
+        assertArrayEquals(currentPaletteRGB3, rgb3);
+    }
+
+
+}
