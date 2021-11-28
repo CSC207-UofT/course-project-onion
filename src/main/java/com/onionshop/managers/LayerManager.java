0f8ba62265aa8fdd593897f2f12e63aa4a76d810
Runce Zhang <runce001@gmail.com>
2021-11-27 22:41:49 -0500

    Small corrections to LayerManager.java. Got rid of the check/uncheck and added selectedLayer

--- src/main/java/com/onionshop/managers/LayerManager.java
+++ src/main/java/com/onionshop/managers/LayerManager.java
@@ -11,21 +11,23 @@ public class LayerManager{
      * Save and manage layers created in an onion file
      */
 
-    // the list of layers (layer ~ 2D Pixel array)
-    private List<Pixel[][]> layers;
-    private List<Pixel[][]> hiddenLayers;
 
+    // the list of layers (layer ~ Project objects)
+    private List<Project> layers;
+
+    // the current layer selected by the user
+    private Project selectedLayer;
 
     public LayerManager(){
         this.layers = new ArrayList<>();
-        this.hiddenLayers = new ArrayList<>();
+
     }
 
     /**
      * When the user creates a new layer, add a new blank canvas to the layers list
      * @param layer : the new layer to be added
      */
-    public void addLayer(Pixel[][] layer){
+    public void addLayer(Project layer){
         this.layers.add(layer);
     }
 
@@ -34,37 +36,20 @@ public class LayerManager{
      * @param layer: the new layer to be removed
      * Precondition: layer in layers
      */
-    public void removeLayer(Pixel[][] layer){
+    public void removeLayer(Project layer){
         this.layers.remove(layer);
 
     }
 
     /**
-     * When the user unchecks a layer, make it invisible by storing its pixel values and then set it full white
-     * @param layer: the layer to be hidden
+     * Set selected layer so that when user draws on the board, he is drawing on the correct layer
+     * @param layer
      * Precondition: layer in layers
      */
-    public void uncheckLayer(Pixel[][] layer){
-        // temporary layer that stores the pixel values of the given layer
-        Pixel[][] tempLayer = new Pixel[layer.length][layer[0].length];
-        for (int x = 0; x < layer.length; x++){
-            for (int y = 0; y < layer[0].length; y++){
-                tempLayer[x][y] = layer[x][y];
-                layer[x][y] = new Pixel(new int[]{255, 255, 255});
-            }
-        }
-        this.hiddenLayers.add(tempLayer);
-
+    public void selectLayer(Project layer){
+        this.selectedLayer = layer;
     }
 
-    /**
-     * When the user checks a layer, make it visible by removing it from the hiddenLayers list and adding it to the layers list
-     * @param layer: the layer to be made visible
-     * Precondition: layer in hiddenLayers
-     */
-    public void checkLayer(Pixel[][] layer){
-        this.hiddenLayers.remove(layer);
-        this.layers.add(layer);
 
-    }
+
 }
