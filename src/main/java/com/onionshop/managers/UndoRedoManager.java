bc6fa281879b71cf788e1b65b33180aab4ec14f4
Tina Zhang <69278622+Gnahzanit@users.noreply.github.com>
2021-11-27 19:27:15 -0500

    change max undoredo saving size to 5 instead of 50

--- src/main/java/com/onionshop/managers/UndoRedoManager.java
+++ src/main/java/com/onionshop/managers/UndoRedoManager.java
@@ -8,7 +8,7 @@ public class UndoRedoManager {
     /**
      * Store up to five steps of Drawing instance and keep track on canvas state.
      */
-    private int maxStackSize = 50 + 1; //number of previous states PLUS the current state (+1)
+    private int maxStackSize = 5 + 1; //number of previous states PLUS the current state (+1)
     private final Stack<DrawingState> undoStack = new Stack<>();
     private final Stack<DrawingState> redoStack = new Stack<>();
 
