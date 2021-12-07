2d5ce4daf21e131619a2938a36b8fd34f05f17d2
camm17 <ammcourtney0@gmail.com>
2021-12-06 19:51:47 -0500

    added remove button

--- src/main/java/com/onionshop/controllers/LayerControlUI.java
+++ src/main/java/com/onionshop/controllers/LayerControlUI.java
@@ -5,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -17,6 +18,7 @@ public class LayerControlUI extends HBox {
     @FXML private Label layerName;
     @FXML private CheckBox layerCheckBox;
     @FXML private final Canvas layer;
+    @FXML private Button removeLayerButton;
     private final int layerIndex;
 
     /**
