9cbf8c1220beb7b330e6d30832b1f449d20e05df
Tina Zhang <69278622+Gnahzanit@users.noreply.github.com>
2021-12-08 15:16:46 -0500

    tool tests all fixed

--- src/test/java/com/onionshop/entities/CircleTest.java
+++ src/test/java/com/onionshop/entities/CircleTest.java
@@ -66,12 +66,16 @@ public class CircleTest {
 
         int[][] expected = new int[][] {
                 {1, 1},{2, 1},{3, 1}, {4, 1}, {2, 4}, {3, 3}, {4, 1},
-                {0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},
+                {4, 2},{3, 3},{1, 4},{1, 2},{1, 3},{1, 4},
+                {0, 0},{0, 0},{0, 0},{0, 0},{0, 0}
         };
 
         circle.calculateEffectedPixels();
 
         int[][] actual = circle.pixelsEffectedByShape;
+
+        System.out.println(Arrays.deepToString(actual));
+
         assertArrayEquals(expected, actual);
     }
 }
